7ef2d167b2b18b965fa5fc3c8dab0d6df03dfd0b
AndyStef <Andy.Stef2512@gmail.com>
2017-01-21 14:57:46 +0200

    Create UIImageView.swift

---
+++ UIImageView.swift
@@ -0,0 +1,17 @@
+extension UIImageView {
+  func loadImageWithURL(_ url: URL) -> URLSessionDownloadTask {
+    let session = URLSession.shared
+    let downloadTask = session.downloadTask(with: url, completionHandler: {
+      [weak self] url, response, error in
+      if error == nil, let url = url, let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
+        DispatchQueue.main.async {
+          if let strongSelf = self {
+            strongSelf.image = image
+          }
+        }
+      }
+    })
+    downloadTask.resume()
+    return downloadTask
+  }
+}
