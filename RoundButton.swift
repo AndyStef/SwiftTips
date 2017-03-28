dbb37540f1ca798b2623b31dc243bfd92a2839c9
AndyStef <Andy.Stef2512@gmail.com>
2017-03-28 14:47:31 +0300

    Create RoundButton.swift

---
+++ RoundButton.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    @IBInspectable var borderWidth: CGFloat = 0.0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+}
