bb3c46e2dceb649ec173492d8bd3ea9a430b67b9
AndyStef <Andy.Stef2512@gmail.com>
2017-03-31 17:54:54 +0300

    Update WrappingArray.swift

--- Algorithms/WrappingArray.swift
+++ Algorithms/WrappingArray.swift
@@ -1,1 +1,26 @@
 
+//start array
+let tracks = ["a", "b", "c", "d", "e"]
+
+//what we should get 
+["d", "e", "a", "b", "c"]
+
+let selectedTrack = "d"
+var playlist = [String]()
+var priorTracks = [String]()
+
+for track in tracks {
+    if track == selectedTrack || playlist.count > 0 {
+        playlist.append(track)
+    } else {
+        priorTracks.append(track)
+    }
+}
+
+playlist.append(contentsOf: priorTracks)
+
+let index = tracks.index(where: {return $0 == selectedTrack})
+let preffix = tracks.prefix(upTo: index!)
+let suffix = tracks.suffix(from: index!)
+
+suffix + preffix
