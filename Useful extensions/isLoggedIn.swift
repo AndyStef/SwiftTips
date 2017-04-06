31944bc8a725032cbd1def0cca38d4c02c233213
AndyStef <Andy.Stef2512@gmail.com>
2017-04-06 19:16:59 +0300

    Create isLoggedIn.swift

---
+++ Useful extensions/isLoggedIn.swift
@@ -0,0 +1,16 @@
+extension UserDefaults {
+    
+    enum UserDefaultsKeys: String {
+        case isLoggedIn
+    }
+    
+    func setIsLoggedIn(value: Bool) {
+        set(value, forKey: UserDefaultsKeys.isLoggedIn.rawValue)
+        synchronize()
+    }
+    
+    func isLoggedIn() -> Bool {
+        return bool(forKey: UserDefaultsKeys.isLoggedIn.rawValue)
+    }
+
+}
