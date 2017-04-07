ba914b5df83b81d4bfa29289cbd0c8674b57404f
AndyStef <Andy.Stef2512@gmail.com>
2017-04-07 23:52:01 +0300

    fixed not working optionals

--- Animations/touches.swift
+++ Animations/touches.swift
@@ -20,51 +20,54 @@ class ViewController: UIViewController {
     let testView = UIView()
     var draggingView: UIView?
     var touchOffset: CGPoint?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let rect = CGRect(x: 130, y: 100, width: 150, height: 150)
         testView.frame = rect
         testView.backgroundColor = .red
         view.addSubview(testView)
     }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first
-        guard let point = touch?.location(in: view) else { return }
-        guard let touchedView = view.hitTest(point, with: event) else { return }
-        
-        if touchedView.isEqual(view) {
-            print("touched self")
-        } else {
+        guard let point = touch?.location(in: view),
+              let touchedView = view.hitTest(point, with: event) else { return }
+
+        if !touchedView.isEqual(view) {
             draggingView = touchedView
-            guard let touchPoint = touch?.location(in: draggingView) else { return }
-            touchOffset = CGPoint(x: draggingView?.bounds.midX ?? 0.0 - touchPoint.x,
-                                  y: draggingView?.bounds.midY ?? 0.0 - touchPoint.y)
+            guard let touchPoint = touch?.location(in: draggingView),
+                  let draged = draggingView else { return }
+            view.bringSubview(toFront: draged)
+            touchOffset = CGPoint(x: draged.bounds.midX - touchPoint.x,
+                                  y: draged.bounds.midY - touchPoint.y)
+        } else {
+            draggingView = nil
         }
     }
-    
+
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let draggingView = draggingView {
             let touch = touches.first
-            guard let point = touch?.location(in: view) else { return }
-            let correctPoint = CGPoint(x: point.x + (touchOffset?.x ?? 20.0),
-                                       y: point.y + (touchOffset?.y ?? 20.0))
+            guard let point = touch?.location(in: view),
+                  let offset = touchOffset else { return }
+            let correctPoint = CGPoint(x: point.x + offset.x,
+                                       y: point.y + offset.y)
             draggingView.center = correctPoint
         }
     }
-    
+
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        //print("touches ended")
         draggingView = nil
         touchOffset = nil
     }
-    
+
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         //that can be done with floor panel or with some gesture i guess
         print("touches canceled")
         draggingView = nil
         touchOffset = nil
     }
+
 }
