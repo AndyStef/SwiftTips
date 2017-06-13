5c58f436dbbf0379ed052e188e3f30b4391b46e5
AndyStef <Andy.Stef2512@gmail.com>
2017-06-13 13:26:43 +0300

    Create RandomNumber.swift

--- RandomNumber.swift
+++ RandomNumber.swift
@@ -20,3 +20,11 @@ let random = arc4random_uniform(10)
 let random = arc4random_uniform(11) + 10 
 //generate random number from 10 to 20 
 //default starting value for this func is 0 and we need to plus to get bigger starting value
+
+
+
+//Random color 
+let red =  1 + drand48() * 255 
+let green =  1 + drand48() * 255 
+let blue =  1 + drand48() * 255 
+let randomColor = UIColor(colorLiteralRed: Float(red/255), green: Float(green/255), blue: Float(blue/255), alpha: 1.0)
