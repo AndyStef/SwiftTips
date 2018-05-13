54c71f97f305be6741e080d0b7875dea1b94b3ed
AndyStef <Andy.Stef2512@gmail.com>
2018-05-13 12:38:46 +0300

    Update GoodCodeIdeas.swift

--- GoodCodeIdeas.swift
+++ GoodCodeIdeas.swift
@@ -230,3 +230,23 @@ class Path {
       .url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
   }
 }
+                                                            
+                                                            
+                                                            
+                                                            
+extension String {
+
+  /// Uses CommonCrypto framework to generate sha512 of the current string
+  var sha512: Data? {
+    let stringData = data(using: String.Encoding.utf8)!
+    var result = Data(count: Int(CC_SHA512_DIGEST_LENGTH))
+    _ = result.withUnsafeMutableBytes { resultBytes in
+      stringData.withUnsafeBytes { stringBytes in
+        CC_SHA512(stringBytes, CC_LONG(stringData.count), resultBytes)
+      }
+    }
+
+    return result
+  }
+}
+
